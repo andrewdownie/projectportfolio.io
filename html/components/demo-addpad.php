33720c6b176aebf1b2c5514983581b1d9357170c
Andrew Downie <dd_downie@live.ca>
2016-01-25 20:44:00 +0000

    Updated the look of the addpad, got the add pad following mouse click. Played around a little with how text fields will be styled.

---
+++ html/components/demo-addpad.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Demo: AddPad</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS
+    <link href="css/shop-item.css" rel="stylesheet"> TODO: DELETE THIS-->
+    <link href="/css/navbar.css" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="/js/lib/jquery-2.2.0.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/lib/bootstrap.min.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        textarea{
+            border: none;
+            resize: none;
+        }
+        textarea:hover{
+            background-color: #ddd;
+        }
+    </style>
+
+    <script>
+
+    </script>
+</head>
+
+<body>
+
+    <!-- Page Content -->
+    <div class="container">
+        <p>Click here.</p>
+
+        <!--<textarea></textarea>-->
+        <textarea></textarea>
+
+        <?php include "/var/www/projectportfolio/html/components/addpad.html";?>
+
+
+
+    </div>
+</body>
+
+</html>
