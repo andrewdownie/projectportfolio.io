9b029a9685c81170f3305a93f4e492c95b6d342b
Andrew Downie <dd_downie@live.ca>
2016-01-26 19:47:09 +0000

    Did some work on the style pad, doesn't look quite as good as I'd like

--- html/blogeditor.php
+++ html/blogeditor.php
@@ -76,7 +76,7 @@
         <div class="row">
             <div class="col-md-2" style="position:fixed;">
                 <?php include "/var/www/projectportfolio/html/components/addpad.html";?>
-                <?php include "/var/www/projectportfolio/html/components/addpad.html";?>
+                <?php include "/var/www/projectportfolio/html/components/stylepad.html";?>
             </div>
             <div class="col-md-8 col-md-offset-2">
                 <button id="red">Red</button>
