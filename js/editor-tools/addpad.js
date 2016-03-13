7997d44de6c8cc61916c8610002f34f0dd8396fc
Andrew Downie <dd_downie@live.ca>
2016-03-13 16:35:00 +0000

    Made it so the element you mousedown on is the selected element, instead of requring both mousedown and mouse up on the same element. Much better for usability.

--- js/editor-tools/addpad.js
+++ js/editor-tools/addpad.js
@@ -2,45 +2,54 @@ $(document).ready(function() {
     var focusElement
 
     //TODO: change "#addpad-title" to "#addpad #title"
-    $('#addpad #title').click(function(e){
+    $('#addpad #title').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
         focusElement.after("<p contenteditable='true' class='title'> title </p>").next("p").focus()
+        return false;
     });
 
-    $('#addpad #text').click(function(e){
+    $('#addpad #text').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
         focusElement.after("<p contenteditable='true'> text </p>").next("p").focus()
+        return false;
     });
 
-    $('#addpad #image').click(function(e){
+    $('#addpad #image').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
         focusElement.after("<img contenteditable='true' style='width: 100%; min-width: 100%;' src='https://picjumbo.imgix.net/HNCK0852.jpg?q=40&w=1650&sharp=30' alt='Image, pls'> </img>").next("img").focus()
 
+        return false;
     });
 
+    $('body').on('mousedown', function(e){
 
-    $('body').on('click', function(e){
+
         if($(e.target).is("img[contenteditable=true]")){
-            focusElement = $(":focus")
-            $("#addpad table tr#enabled").show()
-            $("#addpad table tr#disabled").hide()
+            focusElement = $(e.target)
+            EnableAddpad()
 
         }
         else if($(e.target).is("p[contenteditable=true]")){
-            focusElement = $(":focus")
-            $("#addpad table tr#enabled").show()
-            $("#addpad table tr#disabled").hide()
+            focusElement = $(e.target)
+            EnableAddpad()
         }
         else{
             focusElement = null
-            $("#addpad table tr#disabled").show()
-            $("#addpad table tr#enabled").hide()
+            DisableAddpad()
         }
 
 
     });
 
 });
+function EnableAddpad(){
+    $("#addpad table tr#enabled").show()
+    $("#addpad table tr#disabled").hide()
+}
+function DisableAddpad(){
+    $("#addpad table tr#disabled").show()
+    $("#addpad table tr#enabled").hide()
+}
