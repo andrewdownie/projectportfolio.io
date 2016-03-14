ec09cdd0a09157503570fa103de2b814eb8c2e35
Andrew Downie <dd_downie@live.ca>
2016-03-14 03:40:57 +0000

    Fixed an issue where adding a blog element while having a style span in a contenteditable paragraph, would cause that paragraph to get split into two seperate elements, and the added element would go inbetween. I'll start the login system some day...

--- js/editor-tools/addpad.js
+++ js/editor-tools/addpad.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
     $('#addpad #title').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
-        focusElement.after("<p contenteditable='true' class='title'> <span class='bold'> title </span> </p>").next("p").focus()
+        GetNearestEditableP(focusElement).after("<p contenteditable='true' class='title'> <span class='bold'> title </span> </p>").next("p").focus()
         return false;
     });
 
     $('#addpad #text').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
-        focusElement.after("<p contenteditable='true'> text </p>").next("p").focus()
+        GetNearestEditableP(focusElement).after("<p contenteditable='true'> text </p>").next("p").focus()
         return false;
     });
 
     $('#addpad #image').mousedown(function(e){
         $(this).blur()
         if(typeof(focusElement) === 'undefined'){ return; }
-        focusElement.after("<img contenteditable='true' style='width: 100%; min-width: 100%;' src='https://picjumbo.imgix.net/HNCK0852.jpg?q=40&w=1650&sharp=30' alt='Image, pls'> </img>").next("img").focus()
+        GetNearestEditableP(focusElement).after("<img contenteditable='true' style='width: 100%; min-width: 100%;' src='https://picjumbo.imgix.net/HNCK0852.jpg?q=40&w=1650&sharp=30' alt='Image, pls'> </img>").next("img").focus()
 
         return false;
     });
@@ -52,4 +52,12 @@ function EnableAddpad(){
 function DisableAddpad(){
     $("#addpad table tr#disabled").show()
     $("#addpad table tr#enabled").hide()
+}
+
+function GetNearestEditableP(focusElement){
+    if(focusElement.is("[contenteditable=true]")){
+        return focusElement
+    }
+
+    return focusElement.parents("[contenteditable=true]")
 }
