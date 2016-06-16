885162feb79b481593375af638b6ae059153b568
Andrew Downie <andrew_dev@outlook.com>
2016-06-16 17:12:26 +0000

    An email is sent when a user signs up, but the code sent seems to be wrong.

--- backend/functions.php
+++ backend/functions.php
@@ -34,80 +34,113 @@ function validate_password($password){
     $invalidCharacters = "([^A-Za-z0-9!()$%^&*+=.?\[\]_`~@# |,<>:;{}-])";
 
 
-    if(preg_match($invalidCharacters, $password) === 1){
-        return "invalid-password-characters";
+        if(preg_match($invalidCharacters, $password) === 1){
+            return "invalid-password-characters";
+        }
+
+        if(preg_match($eightToTwenty, $password) === 0){
+            return "invalid-password-length";
+        }
+
+        if(preg_match($uppercase, $password) === 0){
+            return "invalid-password-uppercase";
+        }
+
+        if(preg_match($lowercase, $password) === 0){
+            return "invalid-password-lowercase";
+        }
+
+        if(preg_match($number, $password) === 0){
+            return "invalid-password-number";
+        }
+
+        if(preg_match($symbol, $password) === 0){
+            return "invalid-password-symbol";
+        }
+
+
+        return "valid-password";
     }
 
-    if(preg_match($eightToTwenty, $password) === 0){
-        return "invalid-password-length";
+    function validate_username($username){
+        $fourToFifteen = "(^(.{4,15}$))";
+        $underDotStart = "(^([_.]))";
+        $doubleUnderDot = "(.*[_.]{2})";
+        $underDotEnd = "(([_.])$)";
+        $allowedCharacters = "(^(([a-zA-Z0-9._])+)$)";
+
+        if(preg_match($fourToFifteen, $username) === 0){
+            return "invalid-username-length";
+        }
+
+        if(preg_match($underDotStart, $username) === 1){
+            return "invalid-username-dotunder-start";
+        }
+
+        if(preg_match($doubleUnderDot, $username) === 1){
+            return "invalid-username-double-dotunder";
+        }
+
+        if(preg_match($underDotEnd, $username) === 1){
+            return "invalid-username-dotunder-end";
+        }
+
+        if(preg_match($allowedCharacters, $username) === 1){
+            return "valid-username";
+        }
+
+        return "invalid-username-allowed-characters";
     }
 
-    if(preg_match($uppercase, $password) === 0){
-        return "invalid-password-uppercase";
+    function timestampify($time){
+        return date('m/d/Y h:i:s', $time);
     }
 
-    if(preg_match($lowercase, $password) === 0){
-        return "invalid-password-lowercase";
-    }
-
-    if(preg_match($number, $password) === 0){
-        return "invalid-password-number";
-    }
-
-    if(preg_match($symbol, $password) === 0){
-        return "invalid-password-symbol";
+    function encrypt_password($password) {
+        //TAKEN FROM: https://www.sitepoint.com/password-hashing-in-php/
+        if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+            $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+            return crypt($password, $salt);
+        }
     }
 
 
-    return "valid-password";
-}
-
-function validate_username($username){
-    $fourToFifteen = "(^(.{4,15}$))";
-    $underDotStart = "(^([_.]))";
-    $doubleUnderDot = "(.*[_.]{2})";
-    $underDotEnd = "(([_.])$)";
-    $allowedCharacters = "(^(([a-zA-Z0-9._])+)$)";
-
-    if(preg_match($fourToFifteen, $username) === 0){
-        return "invalid-username-length";
+    function check_password($password, $hashedPassword) {
+        //TAKEN FROM: https://www.sitepoint.com/password-hashing-in-php/
+        return crypt($password, $hashedPassword) == $hashedPassword;
     }
 
-    if(preg_match($underDotStart, $username) === 1){
-        return "invalid-username-dotunder-start";
+    function test_email($signupString){
+        // multiple recipients
+        $to  = 'andrew_dev@outlook.com' . ', '; // note the comma
+        $to .= 'dd_downie@live.ca';
+
+        // subject
+        $subject = 'Complete Registration - ProjectPortfolio';
+
+        // message
+        $message = '
+        <html>
+        <head>
+        <title>Here is your link to complete signup:</title>
+        </head>
+        <body>
+        '.$signupString.'
+        </body>
+        </html>
+        ';//how do i put links in an email?
+
+        // To send HTML mail, the Content-type header must be set
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+        // Additional headers
+        $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+        $headers .= 'From: Signup Robot <signup_robot@projectportfolio.io>' . "\r\n";
+        $headers .= '' . "\r\n";
+        $headers .= '' . "\r\n";
+
+        // Mail it
+        mail($to, $subject, $message, $headers);
     }
-
-    if(preg_match($doubleUnderDot, $username) === 1){
-        return "invalid-username-double-dotunder";
-    }
-
-    if(preg_match($underDotEnd, $username) === 1){
-        return "invalid-username-dotunder-end";
-    }
-
-    if(preg_match($allowedCharacters, $username) === 1){
-        return "valid-username";
-    }
-
-    return "invalid-username-allowed-characters";
-}
-
-function timestampify($time){
-    return date('m/d/Y h:i:s', $time);
-}
-
-function encrypt_password($password) {
-    //TAKEN FROM: https://www.sitepoint.com/password-hashing-in-php/
-    if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
-        $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
-        return crypt($password, $salt);
-    }
-}
-
-
-function check_password($password, $hashedPassword) {
-    //TAKEN FROM: https://www.sitepoint.com/password-hashing-in-php/
-    return crypt($password, $hashedPassword) == $hashedPassword;
-}
-
-?>
+    ?>
