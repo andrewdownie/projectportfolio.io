5f9b059629937637186a629a9acd36dd6b38be76
Andrew Downie <andrew_dev@outlook.com>
2016-06-25 05:15:01 +0000

    Test versions of the core url RewriteRules are in place.

---
+++ html/user.php
@@ -0,0 +1,52 @@
+<?php
+include("/var/www/projectportfolio/html/head.php");
+?>
+
+    <!-- PAGE SPECIFIC INFO -->
+    <title>User - ProjectPortfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <!-- PAGE SPECIFIC INCLUDES -->
+    <link href="/css/login.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include "/var/www/projectportfolio/navbar/navbar.php";?>
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+        <div class="container">
+            <?php include "/var/www/projectportfolio/html/navbar/navbar.php";?>
+        </div>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <!--Home-->
+                    <?php
+                        print_r($_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
+                        echo "<br/>";
+                        print_r($_SERVER['QUERY_STRING']);
+                    ?>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+
+
+</body>
+
+</html>
