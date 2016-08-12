bc32269dbeb73737890ff3e2c0fa17827a453073
Andrew Downie <andrew_dev@outlook.com>
2016-08-12 04:16:18 +0000

    The blog number of the blog the user is editing is now found. Now to update the tables for that blog.

--- backend/editor.php
+++ backend/editor.php
@@ -1,34 +1,52 @@
 <?php
 
 function save_blog($projectUrlName, $blogUrlName){
-    //1. get the blog that matches the user and project url name
+    //1. get the project number for the blog the user is currently editing
     //2. once that is had... update the blog contents, 
     //3. then update the blog title, the blog desciption, the blog image, and the blog modified date
     
-   // echo "You sent " . $projectUrlName . " and " . $blogUrlName . " to SaveBlog";
     $currentUser = current_account();
 
-    //1----
-    // get the blog number for the project with the blogUrlName, and the current user as the owner
-    $selectProjectNum = "SELECT * FROM project_head WHERE owner=$currentUser, url_name=$projectUrlName";
-    $resultProjectNum = query($selectBlogNum);
-    echo $resultProjectNum;
 
-    if(mysqli_num_rows($resultProjectNum != 1){
-        echo "{'result': 'error-saving-blog-1'}";
+    // 1 ----- Get the project number for the blog the user is currently editing
+    $selectProjectNum =  "SELECT project_head.project FROM project_head INNER JOIN project_info ON project_head.project = project_info.project"; 
+    $selectProjectNum .= " WHERE project_head.owner = $currentUser AND project_info.url_name = '$projectUrlName'";
+
+    $resultProjectNum = query($selectProjectNum);
+
+    if($resultProjectNum == false || mysqli_num_rows($resultProjectNum) != 1){
+        print_r($resultProjectNum);
+        echo "{'result': 'error-saving-blog'}";
+        return;
+
+    }
+
+    $row = mysqli_fetch_assoc($resultProjectNum);
+
+    $projectNum = $row['project'];
+
+
+    // 2 ----- Get the blog number the user is currently editing
+    $selectBlogNum = "SELECT blog FROM blog_head WHERE project=$projectNum AND url_name='$blogUrlName';";
+    $resultBlogNum = query($selectBlogNum);
+
+
+    if($resultBlogNum == false || mysqli_num_rows($resultBlogNum) != 1){
+        echo "{'result': 'error-saving-blog'}";
         return;
     }
 
-    $projectHeadRow = mysqli_fetch_assoc($resultProjectNum);
-    $projectNum = $projectHeadRow['project'];
+    $row = mysqli_fetch_assoc($resultBlogNum);
+    $blogNum = $row['blog'];
 
-    $selectBlogNum = "SELECT * FROM blog_head WHERE project=$projectNum, url_name=$blogUrlName";
-    $resultBlogNum = query($resultBlogNum);
 
-    //if this number isnt null, then we found the blog the user is currently editing
+    echo "blog num is: " . $blogNum;
 
-    //2----
-    // user the blog number from above to update stuff
+
+
+
+
+
 
 }
 
